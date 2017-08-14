a8df8fdd8fb1aa2f1a4c9fd800d043c772e6d8ae
araby909 <30875419+araby909@users.noreply.github.com>
2017-08-14 09:04:51 -1000

    Add files via upload

--- script.js
+++ script.js
@@ -1,8 +1,11 @@
 function sayHello() {
   //i create a function called sayHello//
-  var userName = prompt("What is your name?");
+  var userName = prompt("what is your first name?");
+    var userName2 = prompt("what is your surname?");
+      var userName3 = prompt("what is your nickname?");
   //i create a variable called userName and ask the user something//
   window.alert("Hello " + userName);
+    window.alert("wasssssssuppp " + userName2);
+      window.alert("Hitmonlee " + userName3);
   //i create a popup window telling the userName hello.//
 }
-
